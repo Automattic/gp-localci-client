69cfbfb6f3e2a12d61d1c343fd3b0adfbf81e5fd
Jesús Amieiro <1667814+amieiro@users.noreply.github.com>
2025-08-20 18:09:45 +0200

    Get the changes from the remote branch at the beginning of the extraction method

--- generate-new-strings-pot.sh
+++ generate-new-strings-pot.sh
@@ -70,11 +70,14 @@ function extract_php_strings() {
 	DEFAULT_POT="build/pot/localci-default-branch-php-strings.pot"
 	OUTPUT_POT="build/pot/localci-new-php-strings.pot"
 	git config pull.ff only
+	# Syncronize the default branch with the remote 
+	git checkout $DEFAULT_BRANCH
+	git merge --ff-only origin/$DEFAULT_BRANCH
 #	git checkout $DEFAULT_BRANCH
 #	git pull origin $DEFAULT_BRANCH
 #	echo "Current branch: $(git rev-parse --abbrev-ref HEAD)" 
 #	echo "SHA of the last commit of the $(git rev-parse --abbrev-ref HEAD) branch: $(git rev-parse HEAD)"
-#	git checkout $BRANCH
+	git checkout $BRANCH
 
 	echo "Show the graph of the last 400 commits:"
 	git log --graph --oneline --all -n 400
@@ -103,9 +106,7 @@ function extract_php_strings() {
 	clean_pot_headers "$NEW_POT"
 	echo "Extraction complete. Output: $NEW_POT"
 
-	# Syncronize the default branch with the remote 
 	git checkout $DEFAULT_BRANCH
-	git merge --ff-only origin/$DEFAULT_BRANCH || true
 	echo "Start the string extraction for default branch"
 	if [ -n "$CHANGED_PHP_FILES" ]; then
 		wp i18n make-pot . "$DEFAULT_POT" --ignore-domain --skip-audit --include="$CHANGED_PHP_FILES" --debug
