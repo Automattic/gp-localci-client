a7974c3e542e5d9da3ef701002821a28ce554694
Jesús Amieiro <1667814+amieiro@users.noreply.github.com>
2025-08-20 19:35:02 +0200

    Update the remote another time

--- generate-new-strings-pot.sh
+++ generate-new-strings-pot.sh
@@ -72,8 +72,7 @@ function extract_php_strings() {
 	git config pull.ff only
 	# Syncronize the default branch with the remote 
 	git checkout $DEFAULT_BRANCH
-	git merge --ff-only origin/$DEFAULT_BRANCH
-	git pull origin/$DEFAULT_BRANCH $DEFAULT_BRANCH
+	git pull origin $DEFAULT_BRANCH
 #	echo "Current branch: $(git rev-parse --abbrev-ref HEAD)" 
 #	echo "SHA of the last commit of the $(git rev-parse --abbrev-ref HEAD) branch: $(git rev-parse HEAD)"
 	git checkout $BRANCH
